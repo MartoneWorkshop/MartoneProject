9a09feb5c273e8fecf995a58c6c0d8623990d72c
Martone Laptop <martoneworkshop@gmail.com>
2024-08-30 23:19:42 -0400

    Correcion de errores al subir archivos, vuelta a sqlite como db Punto a continuar, definicion de grupos para articulos

--- functions/conexion.py
+++ functions/conexion.py
@@ -1,41 +1,31 @@
-import psycopg2
+import sqlite3
 from tkinter import messagebox
 
 class ConexionDB:
+    
     def __init__(self):
-        self.host = 'localhost'
-        self.database = 'postgres'
-        self.user = 'postgres'
-        self.password = 'admin'
-        self.port = '5432'
-        try:
-            self.conexion = psycopg2.connect(
-                host=self.host,
-                database=self.database,
-                user=self.user,
-                password=self.password,
-                port=self.port
-            )
-            self.cursor = self.conexion.cursor()
-        except psycopg2.Error as error:
-            messagebox.showerror("Error de conexion", f"No se pudo conectar a la base de datos: {str(error)}")
+        self.baseDatos = 'database/database.db'
+        self.conexion = sqlite3.connect(self.baseDatos)
+        self.cursor = self.conexion.cursor()
+
     def cerrarConexion(self):
         self.conexion.commit()
         self.conexion.close()
+        
     def ejecutar_consulta(self, consulta):
         try:
             self.cursor.execute(consulta)
             self.conexion.commit()
-        except psycopg2.Error as error:
+        except sqlite3.Error as error:
             messagebox.showerror("Error", f"Error en la consulta: {str(error)}")
-    
+            
     def ejecutar_consulta_parametros(self, consulta, parametros):
         try:
             self.cursor.execute(consulta, parametros)
             self.conexion.commit()
-        except psycopg2.Error as error:
+        except sqlite3.Error as error:
             messagebox.showerror("Error", f"Error en la consulta: {str(error)}")
-
+    
     def obtener_resultado(self):
         resultado = self.cursor.fetchone()
         return resultado
